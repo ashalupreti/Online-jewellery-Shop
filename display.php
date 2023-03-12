4311d7c1c922eab86a50100420ada039d8cc90fa
Ashal <ashalupreti33@gmail.com>
2023-03-12 21:08:52 +0545

    ddd

--- display.php
+++ display.php
@@ -92,7 +92,7 @@
       </div>
       <hr>
       <!-- Copright section -->
-      <p class="copyright">© 2022 License Shakya jewellery. All rights reserved.</p>
+      <p class="copyright">© 2023 License Shakya jewellery. All rights reserved.</p>
     </div>
   </div>
 
