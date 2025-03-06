736162b843064ec7514dc2aac92bff10720cfb7f
Soondy <90265701+ashalupreti@users.noreply.github.com>
2025-03-06 07:03:44 +0545

    Update userinfo.php

--- userinfo.php
+++ userinfo.php
@@ -18,20 +18,8 @@ if($con){
  $address=$_POST['address'];
  $code=$_POST['code'];
  $jewellery=$_POST['jewellery'];
- $file=$_FILES['image'];
  $comments = $_POST['comments'];
 
-//print_r($file);
-$filename=$file['name'];
-$filepath=$file['tmp_name'];
-$fileerror=$file['error'];
-
- if($fileerror==0){
-	 $destfile='upload/'.$filename;
-	 //echo "$destfile";
-	 move_uploaded_file($filepath,$destfile);
-
- }
 $query = "insert into userinfodata(user, email, mobile,address,code,citizenship,interestedon, image, comment) values('$user', '$email', '$mobile','$address','$code','$citizen','$jewellery', '$destfile', '$comments')";
 
   echo "$query";
